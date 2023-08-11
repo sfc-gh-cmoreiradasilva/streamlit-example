253bd90537113e8355d4dca5f021f76fd62f7686
Cris Silva <c.moreiradasilva@snowflake.com>
2023-08-11 20:06:55 +0000

    from codespaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit. Now on GitHub Codespaces!!!
+# Welcome to Streamlit from codespaces!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
